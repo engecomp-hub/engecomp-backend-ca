f53d0f176fc81fbe1e52ef47ac898588cb13476e
buguno <brunnodesouzabezerra@gmail.com>
2025-03-18 16:19:52 -0300

    style(basededadoscaepi): formatting file

--- app/Services/BaseDadosCaEPI.py
+++ app/Services/BaseDadosCaEPI.py
@@ -1,14 +1,16 @@
 import ftplib
 import io
-import zipfile
-import pandas as pd
 import os
 import re
+import zipfile
+
+import pandas as pd
+
 
 class BaseDadosCaEPI:
-    baseDadosDF = None 
+    baseDadosDF = None
     nomeArquivoBase = 'tgg_export_caepi.txt'
-    nomeArquivoErros = 'CAs_com_erros.txt'    
+    nomeArquivoErros = 'CAs_com_erros.txt'
     urlBase = 'ftp.mtps.gov.br'
     caminho = 'portal/fiscalizacao/seguranca-e-saude-no-trabalho/caepi/'
     nColunas = 19
@@ -32,13 +34,15 @@ class BaseDadosCaEPI:
         arquivoZip = zipfile.ZipFile(r)
 
         arquivoZip.extractall()
-    
-    def _transformarEmDataFrame(self):          
+
+    def _transformarEmDataFrame(self):
         listaCas = self._retornarCAsSemErros()
         cols = listaCas[0]
         self.baseDadosDF = pd.DataFrame(listaCas, columns=cols)
-        if('RegistroCA' not in self.baseDadosDF.columns):
-            self.baseDadosDF = self.baseDadosDF.rename(columns = {'#NRRegistroCA':'RegistroCA'})        
+        if 'RegistroCA' not in self.baseDadosDF.columns:
+            self.baseDadosDF = self.baseDadosDF.rename(
+                columns={'#NRRegistroCA': 'RegistroCA'}
+            )
 
     def _retornarCAsSemErros(self) -> list:
         listaCAsValidos = []
@@ -54,40 +58,29 @@ class BaseDadosCaEPI:
                 else:
                     listaCAsInvalidos.append(linha)
                     continue
-    
+
             listaCAsValidos.append(linhaDf)
-        
+
         if listaCAsInvalidos:
             self._criarArquivoComErros(listaCAsInvalidos)
         return listaCAsValidos
-    
+
     def _tratarCasComErros(self, linha) -> dict:
         linhaDf = re.split(r'(?<! )\|', linha)
-        if len(linhaDf) > self.nColunas: # Erro
-            return {
-                'sucess': False,
-                'linha': linha
-            }
+        if len(linhaDf) > self.nColunas:  # Erro
+            return {'sucess': False, 'linha': linha}
 
-        return    {
-            'sucess': True,
-            'linha': linhaDf
-        }
+        return {'sucess': True, 'linha': linhaDf}
 
-    def _criarArquivoComErros(self, listaCAsInvalidos:list) -> None:
+    def _criarArquivoComErros(self, listaCAsInvalidos: list) -> None:
         with open(self.nomeArquivoErros, 'w') as f:
             f.writelines(listaCAsInvalidos)
-    
+
     def retornarBaseDados(self) -> pd.DataFrame:
         if not os.path.exists(self.nomeArquivoBase):
-            print("Aguarde o download...")        
+            print('Aguarde o download...')
             self._baixarArquivoBaseCaEPI()
-            print(f"Download concluido!")
+            print('Download concluido!')
 
         self._transformarEmDataFrame()
         return self.baseDadosDF
-
-    
-
-
-   
