97c549c4140f805b95d56632343a91c3bcef50d7
buguno <brunnodesouzabezerra@gmail.com>
2025-03-18 16:20:35 -0300

    style(main): formatting file

--- app/main.py
+++ app/main.py
@@ -1,35 +1,36 @@
-from fastapi import Depends, FastAPI
 import uvicorn
-from routers import certificadoApovacao
-
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import certificadoApovacao
 
 tag_ca = [
     {
-        "name": "Certificado de Aprovação",
-        "description": "Consultas e Operações da BaseCAEPI"        
+        'name': 'Certificado de Aprovação',
+        'description': 'Consultas e Operações da BaseCAEPI',
     }
 ]
-app = FastAPI( 
-    title="API BaseCAEPI",
+app = FastAPI(
+    title='API BaseCAEPI',
     description="""Pesquisar e recuperar informações(por json ou arquivo de excel) sobre os certificados de aprovação emitidos para EPIs.\n
     Codigo fonte: https://github.com/JoaoAugustoMV/API_BaseCAEPI
         """,
-        openapi_tags=tag_ca
-    )
+    openapi_tags=tag_ca,
+)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 app.include_router(certificadoApovacao.router)
 
-@app.get("/", tags=["HOME"])
-async def index():
-    return "HOME"
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.get('/', tags=['HOME'])
+async def index():
+    return 'HOME'
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
